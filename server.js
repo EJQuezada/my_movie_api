4afb0d558526239a2da04b5d86e5f7d7a4dfb277
EJQuezada <equezada@msn.com>
2023-01-23 10:24:46 +0100

    udpate user function with if-else function

--- server.js
+++ server.js
@@ -100,6 +100,13 @@ app.put('/users/:id', (req, res) => {
 
     let user = users.find( user => user.id == id );
 
+    if (user) {
+        user.Name = updatedUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send('no such user')
+    }
+
 })
 
 //READ
