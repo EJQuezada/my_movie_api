2ff12a58c2e9644c9e0bbdf1f2b2c5f85a5542c7
EJQuezada <equezada@msn.com>
2023-02-11 23:48:00 +0100

    input code to auth.js to create endpoint

---
+++ auth.js
@@ -0,0 +1,35 @@
+const jwtSecret = 'P3rug1@99!';
+
+const jwt = require('jsonwebtoken'),
+    passport = require('passport');
+
+require('./passport');
+
+let gerateJWToken = (user) => {
+    return jwt.sign(user, jwtSecret, {
+        subject: user.Username, //This is the username being encoded in the JWT
+        expiresIn: '7d',
+        algorithm: 'HS256'
+    });
+}
+
+/* POST login. */
+module.exports = (router) => {
+    router.post('/login', (req, res) => {
+        passport.authenticate('local', { session: false }, (error, user, info) => {
+            if (error || !user) {
+                return res.status(400).json({
+                    message: 'Something is not right',
+                    user: user
+                });
+            }
+            req.login(user, { session: false }, (error) => {
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
+            });
+        }) (req, res);
+    });
+}
