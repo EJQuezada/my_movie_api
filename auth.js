0f0db89985374a6963a545bd18e0678fdd2bc381
EJQuezada <equezada@msn.com>
2023-02-15 15:36:03 +0100

    update auth.js

--- auth.js
+++ auth.js
@@ -20,7 +20,7 @@ module.exports = (router) => {
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Something is not right',
-                    user: user
+                    user: user,
                 });
             }
             req.login(user, { session: false }, (error) => {
