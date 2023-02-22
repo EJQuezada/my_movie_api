073d7c3d7afd2702827870004125dc889cc54b51
EJQuezada <equezada@msn.com>
2023-02-22 21:42:50 +0100

    add cors function to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const uuid = require('uuid');
 const morgan = require('morgan');
