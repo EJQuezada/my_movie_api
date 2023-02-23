0d8ef11f2151bb83cdb4cedc730428b4de2833b8
EJQuezada <equezada@msn.com>
2023-02-23 12:03:59 +0100

    add hashed passport to index.js, passport.js files

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) 
 
 //CREATE allows new users to register
 app.post('/users', (req, res) => {
+    let hashedPassword = Users.hashedPassword(req.body.Password);
     Users.findOne({ Username: req.body.Username })
        .then((user) => {
             if (user) {
@@ -50,7 +51,7 @@ app.post('/users', (req, res) => {
                 Users
                     .create({
                         Username: req.body.Username,
-                        Password: req.body.Password,
+                        Password: hashedPassword,
                         Email: req.body.Email, 
                         Birthday: req.body.Birthday
                     })
@@ -58,7 +59,7 @@ app.post('/users', (req, res) => {
                 .catch((error) => {
                     console.error(error);
                     res.status(500).send('Error: ' + error);
-                })
+                });
             }
         })
         .catch((error) => {
--- passport.js
+++ passport.js
@@ -21,7 +21,7 @@ passport.use(new LocalStrategy({
 
         if (!user) {
             console.log('incorrect username');
-            return callback(null, false, {message: 'Incorrect username or password.'});
+            return callback(null, false, {message: 'Incorrect username.'});
         }
 
         if (!user.validatePassword(password)) {
